d6fdb41c510ac72710c9998b7c54efc33d7a0450
Ernar <aseeayou@mail.ru>
2021-02-02 20:24:53 +0600

    TSIS2 4th

--- p1.py
+++ p1.py
@@ -1,8 +1,14 @@
 class Solution(object):
-    def numIdenticalPairs(self, nums):
-        sum = 0
-        for i in range(0, len(nums)):
-            for j in range(i + 1, len(nums)):
-                if (nums[i] == nums[j]):
-                    sum = sum + 1
-        return sum
+    def largestAltitude(self, gain):
+        mass = []
+        x = 0
+        mass += [x]
+        for i in gain:
+            mass += [x + i]
+            x = x + i
+        max = -999
+        for i in mass:
+            if i > max:
+                max = i
+        return max
+
