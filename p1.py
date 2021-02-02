4a57ba97b17bea85a14ac79d3092320330d5cbd3
Ernar <aseeayou@mail.ru>
2021-02-02 19:25:23 +0600

    TSIS2 3rd

--- p1.py
+++ p1.py
@@ -1,5 +1,8 @@
 class Solution(object):
-    def interpret(self, command):
-        command = command.replace("()","o")
-        command = command.replace("(al)","al")
-        return command
+    def numIdenticalPairs(self, nums):
+        sum = 0
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if (nums[i] == nums[j]):
+                    sum = sum + 1
+        return sum
