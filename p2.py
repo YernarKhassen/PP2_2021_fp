014afbcb17f0b24c5dee16d577191aeb1f3fa9bd
Ernar <aseeayou@mail.ru>
2021-02-22 11:49:50 +0600

    TSIS3 6th

--- p2.py
+++ p2.py
@@ -1,8 +1,1 @@
-a = list(map(int,input().split()))
-n = int(input())
-n%=len(a)
-if n < 0:
-    n = abs(n)
-    print(*(a[n:] + a[0:n]))
-else:
-    print(*(a[-n:] + a[0:-n]))
+print(len(set(map(int,input().split()))))
